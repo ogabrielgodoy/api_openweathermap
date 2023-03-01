d5eb36af768f46bd3a4717dea9627ac2dd91cb8e
Gabriel Godoy <110994759+ogabrielgodoy@users.noreply.github.com>
2023-03-01 19:43:51 -0300

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">
+              <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Início</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+              <i class="icon-head menu-icon"></i>
+              <span class="menu-title">Desenvolvedor</span>
+            </a>
+          </li>
+
+        </ul>
+      </nav>
